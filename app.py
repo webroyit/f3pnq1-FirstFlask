c24d8a6b25221d0923b163b3712aa33f31fe7b28
royweb <xytank@gmail.com>
2020-02-12 14:56:46 -0500

    Configure the database

--- app.py
+++ app.py
@@ -3,12 +3,18 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow 
 import os       # work with files
 
+# init app
 app = Flask(__name__)
-
-# route
-@app.route("/", methods=["GET"])
-def get():
-    return jsonify({ "data": "It works"})
+# find the path to save the file
+basedir = os.path.abspath(os.path.dirname(__file__))
+# accress to the database
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(basedir, "db.sqlite")
+# prevent the warming message
+app.config("SQLALCHEMY_TRACK_MODIFICATIONS") = False
+# init database
+db = SQLAlchemy(app)
+# init marshmallow
+ma = Marshmallow(app)
 
 # start the server
 if __name__ == "__main__":
