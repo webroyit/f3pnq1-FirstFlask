28df3fe18fe91ed425a5c795d28bf1ee6d54a9a0
royweb <xytank@gmail.com>
2020-02-15 10:50:02 -0500

    Add a class for the product

--- app.py
+++ app.py
@@ -10,11 +10,19 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 # accress to the database
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(basedir, "db.sqlite")
 # prevent the warming message
-app.config("SQLALCHEMY_TRACK_MODIFICATIONS") = False
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 # init database
 db = SQLAlchemy(app)
 # init marshmallow
 ma = Marshmallow(app)
+
+#product class or model
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(100), unique = True)
+    description = db.Column(db.String(200))
+    price = db.Column(db.Float)
+    qty = db.Column(db.Ineger)
 
 # start the server
 if __name__ == "__main__":
